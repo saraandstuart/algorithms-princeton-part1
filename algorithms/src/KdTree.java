5659d9b8bd68302cef6ead1a615c3cad7f6db1f7
Stuart Shannon <saraandstuart@gmail.com>
2015-03-03 08:07:02 +0000

    Add simple insert() and contains()
    Simplified version of insert() which does everything except set up the
    RectHV for each node.

--- algorithms/src/KdTree.java
+++ algorithms/src/KdTree.java
@@ -6,71 +6,120 @@
  */
 public class KdTree {
 
-	private static class Node {
-		private Point2D p;      // the point
-		private RectHV rect;    // the axis-aligned rectangle corresponding to this node
-		private Node lb;        // the left/bottom subtree
-		private Node rt;        // the right/top subtree
+    private static class Node {
+        private Point2D p;      // the point
+        private RectHV rect;    // the axis-aligned rectangle corresponding to this node
+        private Node lb;        // the left/bottom subtree
+        private Node rt;        // the right/top subtree
 
-		public Node(Point2D p, RectHV rect) {
-			this.p = p;
-			this.rect = rect;
-		}
-	}
-	
-	private Node root;
-	
-	private int size;
+        public Node(Point2D p, RectHV rect) {
+            this.p = p;
+            this.rect = rect;
+        }
 
-	public KdTree() {
-		size = 0;
-	}
+        private int compare(Point2D that, boolean vertical) {
+            if (vertical) {
+                return Point2D.X_ORDER.compare(p, that);
+            }
+            return Point2D.Y_ORDER.compare(p, that);
+        }
+    }
 
-	public boolean isEmpty() {
-		return size == 0;
-	}
+    private Node root;
 
-	public int size() {
-		return size;
-	}
+    private int size;
 
-	// add the point to the set (if it is not already in the set)
-	public void insert(Point2D p) {
-		if (p == null) {
-			throw new NullPointerException("Atempt to insert null point");
-		}
-	}
+    public KdTree() {
+        size = 0;
+    }
 
-	// does the set contain point p? 
-	public boolean contains(Point2D p) {
-		if (p == null) {
-			throw new NullPointerException();
-		}
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-		return false;
-	}
+    public int size() {
+        return size;
+    }
 
-	// draw all points to standard draw 
-	public void draw() {
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        if (p == null) {
+            throw new NullPointerException("Atempt to insert null point");
+        }
+        root = insert(root, p, true);
+    }
 
-	}
+    private Node insert(Node x, Point2D p, boolean vertical) {
+        if (x == null) {
+            size++;
+            return new Node(p, null);
+        }
 
-	// all points that are inside the rectangle 
-	public Iterable<Point2D> range(RectHV rect) {
-		if (rect == null) {
-			throw new NullPointerException();
-		}
+        int cmp = x.compare(p, vertical);
 
-		return null;
-	}
+        if (goLeft(cmp)) {
+            x.lb = insert(x.lb, p, !vertical);
+        }
+        else if (goRight(cmp)) {
+            x.rt = insert(x.rt, p, !vertical);
+        }
 
-	// a nearest neighbor in the set to point p; null if the set is empty 
-	public Point2D nearest(Point2D p) {
-		if (p == null) {
-			throw new NullPointerException();
-		}
+        return x; // current node's p is equal to insertion node's p
+    }
 
-		return null;
-	}
+    private boolean goLeft(int cmp) {
+        return cmp > 0;
+    }
+
+    private boolean goRight(int cmp) {
+        return cmp < 0;
+    }
+
+    // does the set contain point p? 
+    public boolean contains(Point2D p) {
+        if (p == null) {
+            throw new NullPointerException();
+        }
+        return contains(root, p, true);
+    }
+
+    private boolean contains(Node x, Point2D p, boolean vertical) {
+        if (x == null) return false;
+
+        int cmp = x.compare(p, vertical);
+
+        if (goLeft(cmp)) {
+            return contains(x.lb, p, !vertical);
+        }
+        else if (goRight(cmp)) {
+            return contains(x.rt, p, !vertical);
+        }
+
+        return true; // current node's p is equal to insertion node's p
+    }
+
+
+    // draw all points to standard draw 
+    public void draw() {
+
+    }
+
+    // all points that are inside the rectangle 
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) {
+            throw new NullPointerException();
+        }
+
+        return null;
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty 
+    public Point2D nearest(Point2D p) {
+        if (p == null) {
+            throw new NullPointerException();
+        }
+
+        return null;
+    }
 
 }
--- algorithms/test/KdTreeTest.java
+++ algorithms/test/KdTreeTest.java
@@ -1,3 +1,7 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 /**
@@ -7,42 +11,122 @@ import org.junit.Test;
  */
 public class KdTreeTest {
 
-	@Test(expected=NullPointerException.class)
-	public void throwsNullPointerExceptionIfNullPointPassedToInsert() {
-		//given
-		KdTree kdTree = new KdTree();
-		//when
-		kdTree.insert(null);
-		//then exception
-	}
+    @Test(expected=NullPointerException.class)
+    public void throwsNullPointerExceptionIfNullPointPassedToInsert() {
+        //given
+        KdTree kdTree = new KdTree();
+        //when
+        kdTree.insert(null);
+        //then exception
+    }
 
-	@Test(expected=NullPointerException.class)
-	public void throwsNullPointerExceptionIfNullPointPassedToContains() {
-		//given
-		KdTree kdTree = new KdTree();
-		//when
-		kdTree.contains(null);
-		//then exception
-	}
+    @Test(expected=NullPointerException.class)
+    public void throwsNullPointerExceptionIfNullPointPassedToContains() {
+        //given
+        KdTree kdTree = new KdTree();
+        //when
+        kdTree.contains(null);
+        //then exception
+    }
 
-	@Test(expected=NullPointerException.class)
-	public void throwsNullPointerExceptionIfNullRectPassedToRange() {
-		//given
-		KdTree kdTree = new KdTree();
-		//when
-		kdTree.range(null);
-		//then exception
-	}
+    @Test(expected=NullPointerException.class)
+    public void throwsNullPointerExceptionIfNullRectPassedToRange() {
+        //given
+        KdTree kdTree = new KdTree();
+        //when
+        kdTree.range(null);
+        //then exception
+    }
 
-	@Test(expected=NullPointerException.class)
-	public void throwsNullPointerExceptionIfNullPointPassedToNearest() {
-		//given
-		KdTree kdTree = new KdTree();
-		//when
-		kdTree.nearest(null);
-		//then exception
-	}
-	
-	
+    @Test(expected=NullPointerException.class)
+    public void throwsNullPointerExceptionIfNullPointPassedToNearest() {
+        //given
+        KdTree kdTree = new KdTree();
+        //when
+        kdTree.nearest(null);
+        //then exception
+    }
+
+    @Test
+    public void shouldReturnFalseWhenNonEmpty() {
+        // Given
+        KdTree kdTree = new KdTree();
+        Point2D point2D = new Point2D(0.1, 0.1);
+        kdTree.insert(point2D);
+
+        // When
+        boolean result = kdTree.isEmpty();
+
+        // Then
+        assertFalse(result);
+    }
+    
+    @Test
+    public void shouldReturnTrueWhenEmpty() {
+        // Given
+        KdTree kdTree = new KdTree();
+
+        // When
+        boolean result = kdTree.isEmpty();
+
+        // Then
+        assertTrue(result);
+    }
+    
+    @Test
+    public void shouldReturnCorrectSize() {
+        // Given
+        KdTree kdTree = new KdTree();
+        Point2D point2D = new Point2D(0.1, 0.1);
+        kdTree.insert(point2D);
+        int expected = 1;
+
+        // When
+        int actual = kdTree.size();
+
+        // Then
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void shouldInsertPoint() {
+        // Given
+        KdTree kdTree = new KdTree();
+        assertEquals(0, kdTree.size());
+
+        // When
+        Point2D point2D = new Point2D(0.1, 0.1);
+        kdTree.insert(point2D);
+
+        // Then
+        assertEquals(1, kdTree.size());
+    }
+    
+    @Test
+    public void shouldReturnTrueWhenContainsPoint() {
+        // Given
+        KdTree kdTree = new KdTree();
+        Point2D point = new Point2D(0.1, 0.1);
+        kdTree.insert(point);
+
+        // When
+        boolean result = kdTree.contains(point);
+
+        // Then
+        assertTrue(result);
+    }
+    
+    @Test
+    public void shouldReturnFalseWhenDoesNotContainPoint() {
+        // Given
+        KdTree kdTree = new KdTree();
+        Point2D point = new Point2D(0.1, 0.1);
+
+        // When
+        boolean result = kdTree.contains(point);
+
+        // Then
+        assertFalse(result);
+    }
 
 }
